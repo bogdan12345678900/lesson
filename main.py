6f3250e926ac684067a6a32823e66acc67939ee7
Plosc_ea16 <plosinskijbogdan@gmail.com>
2024-10-23 17:26:36 +0300

    V.0.1.0

---
+++ main.py
@@ -0,0 +1,55 @@
+# num = int(input("num "))
+# for i in range(0,11):
+#     print(f'{i} * {num} = {i * num}')
+# range(start , stop , step)
+# for i in range(1,11):
+#     for j in  range(1,11):
+#         print(f'{i} * {j} = {i * j}')
+#     print()
+# num = int(input('num '))
+# num1 = int(input('num1 '))
+# while num >= num1 or num1 >= num:
+#     num2 = int(input('num2 '))
+#     if  num2>=num and num2 <= num1 :
+#        break
+# # for k in  range(num , num1 + 1):
+# #     if k != num2:
+# #         print(k, end=" ")
+# #     else:
+# #         print(f'!{k}!', end=" ")
+# s = ""
+# for i in range(num, num1 + 1)
+#     if i != num2
+#         s += str(i)
+#     else:
+#         s += "!"+ str(i)+"i"
+# print(s)
+# num = int(input("num"))
+# num1 = int(input("num1"))
+# i = str('*')
+# q = 1
+# while num1 >= q :
+#     for k in range(1, num + 1):
+#         print(i,end=" ")
+#     print()
+#     q += 1
+
+# w = int(input("num"))
+# h = int(input("num1"))
+# for i in range(h):
+#     for j in range(w):
+#         if i == 0 or i ==  h- 1 or j == 0 or j == w-1 :
+#             print('*',end="")
+#         else:
+#             print(" " ,end="")
+#     print()
+# a = 1
+# b = 10
+#
+# for i in range(a, b + 1):
+#     print(i, end=" ")
+#     for j in range(2, i):
+#         if i % j == 0:
+#             break
+#     else:
+#         print(i)
